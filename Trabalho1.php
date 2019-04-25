dba90aa932c82cb8e18892c8f78429f79b3acd70
Joseph Barreto <48127907+JosephBarreto@users.noreply.github.com>
2019-04-25 08:37:07 -0300

    Update Trabalho1.php

--- Trabalho1.php
+++ Trabalho1.php
@@ -3,18 +3,12 @@ print "Digite o 1º numero:";
 $n1 = (int) fgets(STDIN);
 print "Digite o 2º numero:";
 $n2 = (int) fgets(STDIN);
-print "Digite o 3º numero:";
-$n3 = (int) fgets(STDIN);
 
-if($n1>=$n2 and $n1>=$n3){
+if($n1>=$n2){
      print"o maior numero é $n1\n";
 }
-elseif ($n2>=$n1 and $n2>=$n3) {
+elseif ($n2>=$n1 {
     print"o maior numero é $n2\n";
-}
-elseif ($n3>=$n1 and $n2>=$n2) {
-
-    print"o maior numero é $n3\n";
 }
 else {
     print"vc fez caquinha :(";
