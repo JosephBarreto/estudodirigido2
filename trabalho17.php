9af688d309b5d42c407dcc94934915ed4191472b
Joseph Barreto <48127907+JosephBarreto@users.noreply.github.com>
2019-04-25 22:03:35 -0300

    Update trabalho17.php

--- trabalho17.php
+++ trabalho17.php
@@ -4,6 +4,6 @@ $base = (int) fgets(STDIN);
 print"digite expoente:\n";
 $expoente = (int) fgets(STDIN);
 $potencia = $base ** $expoente;
+print"$base^$expoente\n";
 print"resultado = $potencia:\n";
-print"$base^$expoente";
 ?>
