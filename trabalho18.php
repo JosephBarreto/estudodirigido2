c07c2999dc806b5733ab07f2287874aa1ebc06d9
Joseph Barreto <48127907+JosephBarreto@users.noreply.github.com>
2019-04-25 22:15:36 -0300

    Update trabalho18.php

--- trabalho18.php
+++ trabalho18.php
@@ -1,16 +1,21 @@
 <?php
-$cont = 0;
+$cont = 1;
 $contpar = 0;
 $contimpar = 0;
 while($cont<=10){
-$cont = $cont +1;
-print"numero inteiro pfvr
-";
-$var = (int) fgets(STDIN);
-if($var % 2 == 0){
-$contpar = $contpar + 1;
-}else{
-$contimpar = $contimpar + 1;
-}}
-print"n de impares = $contimpar";
-print"n de par = $cont";
+    $cont = $cont +1;
+    print"digite um numero inteiro:";
+    $num = (int) fgets(STDIN);
+    if($num % 2 == 0 and $num!=0){
+    $contpar = $contpar + 1;
+    }
+        elseif($num % 2!=0){
+        $contimpar = $contimpar + 1;
+        }
+        else{
+         #zero nao conta :D
+        }   
+}
+print"n de impares = $contimpar\n";
+print"n de par = $contpar";
+
